49bb2737113ddfef02390bc52b40570c6a98b307
ashishRajak <ashish@oneinfinity.in>
2025-06-23 12:48:08 +0530

    HyperLocalApp

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,6 +1,6 @@
 class Env {
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'https://0c0a-2409-40c4-3032-510f-7e0b-b315-2877-9660.ngrok-free.app',
+    defaultValue: 'https://ad68-2409-40c4-3029-632f-38e0-de7-2eb9-2809.ngrok-free.app',
   );
 }
